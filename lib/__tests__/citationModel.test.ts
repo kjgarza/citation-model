8b644756203a0ff883582e5e5a75d06be81a4459
kjgarza <kgarza@datacite.org>
2021-09-01 17:38:31 +0200

    test

--- lib/__tests__/citationModel.test.ts
+++ lib/__tests__/citationModel.test.ts
@@ -1,8 +1,5 @@
-// import assert from "assert";
-// import citationModel from "../index.js";
-
-// describe('citationModel', () => {
-//   it('has a test', () => {
-//     assert(false, 'citationModel should have a test');
-//   });
-// });
+describe('citationModel', () => {
+  it('has a test', () => {
+    assert(false, 'citationModel should have a test');
+  });
+});
--- lib/__tests__/codemeta.test.ts
+++ lib/__tests__/codemeta.test.ts
@@ -1,7 +1,3 @@
-import CodeMeta from "../codemeta.js";
-import assert from "assert";
-import * as json from "./fixtures/codemeta.json";
-
 describe("codemeta", () => {
   it("has a test", () => {
     var meta = new CodeMeta();
