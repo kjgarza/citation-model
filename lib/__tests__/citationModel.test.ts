7b4b6bb623debbd2c119db41ca84ab9f39657b61
kjgarza <kgarza@datacite.org>
2021-09-01 08:25:01 +0200

    Revert "remove test"
    This reverts commit b758dcef0f9fc9e8f7040e4da72d190a253a2100.

---
+++ lib/__tests__/citationModel.test.ts
@@ -0,0 +1,8 @@
+// import assert from "assert";
+// import citationModel from "../index.js";
+
+// describe('citationModel', () => {
+//   it('has a test', () => {
+//     assert(false, 'citationModel should have a test');
+//   });
+// });
